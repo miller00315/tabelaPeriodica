7a20ac3c2cecc7115e4ace606582f3885da2f602
Miller Cesar Oliveira <miller00315@gmail.com>
2019-05-13 18:53:19 +0000

    tabela periodica json

--- app/controllers/show_element_controller.rb
+++ app/controllers/show_element_controller.rb
@@ -1,38 +1,51 @@
 class ShowElementController < ApplicationController
+
+  require 'json'
+
+  before_action :set_elements, only: [:show]
+
   def show
-    @element = {at_num: 1, symbol: "H", name:"Hidrogênio", number: 1.0008}
+    @element = {at_num: @elements["elements"][params[:element].to_i - 1]["number"], 
+                symbol: @elements["elements"][params[:element].to_i - 1]["symbol"], 
+                name: @elements["elements"][params[:element].to_i - 1]["name"], 
+                number: @elements["elements"][params[:element].to_i - 1]["atomic_mass"]}
     
     @information = {
-      utility_img: "https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-      get_image:"https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-      summary:"Elemento químico de simbolo H, Com um peso atômico de 1,00794 u, o hidrogênio é o elemento mais leve da tabela periódica. 
-      Sua forma monatômica (H) é a substância química mais abundante no Universo, constituindo aproximadamente 75% de toda a massa bariônica.",
-      category: "diametal não metal",
-      discovered_by: "Henry Cavendish",
+      utility_img: @elements["elements"][params[:element].to_i - 1]["utility_img"],
+      get_image: @elements["elements"][params[:element].to_i - 1]["get_img"],
+      summary: @elements["elements"][params[:element].to_i - 1]["summary"],
+      discovered_by: @elements["elements"][params[:element].to_i - 1]["discovered_by"] ? @elements["elements"][params[:element].to_i - 1]["discovered_by"] : 'desconhecido',
       how_produce: "O alumínio ácido de Lewis liga-se ao oxigénio da molécula de água e o alumínio base de Lewis dissocia o 
       átomo de hidrogénio. Se o processo ocorrer uma segunda vez, envolvendo dois outros átomos de alumínio, 
       restarão dois átomos de hidrogénio, que se ligarão para formar uma molécula de hidrogénio na forma de gás (H2).",
-      named_by: "Antoine Lavoisier",
-      phase: "Gas",
+      named_by: @elements["elements"][params[:element].to_i - 1]["named_by"] ? @elements["elements"][params[:element].to_i - 1]["named_by"] : "Desconhecido",
+      phase: @elements["elements"][params[:element].to_i - 1]["phase"],
       utility: "Ela ocorre principalmente em água e na maior parte dos compostos orgânicos e é usado na produção de amoníaco 
       e de outros produtos químicos, na hidrogenação de gorduras e óleos, e na soldagem.
       O Gás Hidrogênio (H2) é explorado para uso em motores a combustão e em células de combustível.",
-      discovery_img:"https://images.findagrave.com/photos250/photos/2017/295/8279_1508761930.jpg",
-      named_img: "https://www.sciencehistory.org/sites/default/files/styles/rte_full_width/public/historical_profile/lavoisier-profile.jpg?itok=10b3E-XN",
+      spectral_img: @elements["elements"][params[:element].to_i - 1]["spectral_img"],
+      discovery_img: @elements["elements"][params[:element].to_i - 1]["discovery_img"],
+      named_img: @elements["elements"][params[:element].to_i - 1]["named_img"],
     }
     
     @properties = {
-      density: 0.08988,
-      boil: 20.271,
-      atomic_mass: 1.008,
-      melt: 13.99,
-      molar_helt: 28.836,
-      electron_configuration: "1s1",
-		  electron_affinity: 72.769,
-		  electronegativity_pauling: 2.20,
+      density: @elements["elements"][params[:element].to_i - 1]["density"],
+      boil: @elements["elements"][params[:element].to_i - 1]["boil"],
+      atomic_mass: @elements["elements"][params[:element].to_i - 1]["atomic_mass"],
+      melt: @elements["elements"][params[:element].to_i - 1]["melt"],
+      molar_helt: @elements["elements"][params[:element].to_i - 1]["molar_heat"],
+      electron_configuration: @elements["elements"][params[:element].to_i - 1]["electron_configuration"],
+		  electron_affinity: @elements["elements"][params[:element].to_i - 1]["electron_affinity"],
+		  electronegativity_pauling: @elements["elements"][params[:element].to_i - 1]["electronegativity_pauling"],
       color_name: "transparent",
-      quote: "https://github.com/Bowserinator/Periodic-Table-JSON/blob/master/PeriodicTableJSON.json",
+      quote: @elements["elements"][params[:element].to_i - 1]["source"],
       hex_color: "#ffffff",
     }
+  end
+
+  def set_elements
+    
+    @elements = JSON.parse(File.read('app/assets/javascripts/periodicTable.json'))
+    
   end
 end
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,7 +7,7 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
-Rails.application.config.assets.precompile += %w(animated_background.js collapse.js)
+Rails.application.config.assets.precompile += %w(animated_background.js collapse.js periodicTable.json)
 
 
 # Precompile additional assets.
